6fe9a34507c0b5a493bb539bfa30c3d426a4db57
Kamil <kamilrusztyn@gmail.com>
2022-01-02 12:55:12 +0100

    zabezpieczenie przed podaniem liczby z poza zakresu

--- ciag.py
+++ ciag.py
@@ -1,16 +1,19 @@
 print ('Podaj liczbę od 1 do 100')
-liczba = int(input())
+liczba = float(input())
 dlugosc = 1
-while liczba:
-    if not liczba % 2:
-        liczba = liczba /2
-       # print (liczba)
-        dlugosc += 1
-    elif liczba % 2:
-        liczba = liczba*3 + 1
-        #print (liczba)
-        dlugosc += 1
-    if liczba == 1:
-       # print ("stop")
-        print (f"długość ciągu Colatza to {dlugosc} liczb")
-        break
+if  1 <= liczba <= 100: 
+    while liczba:
+        if not liczba % 2:
+            liczba = liczba /2
+        # print (liczba)
+            dlugosc += 1
+        elif liczba % 2:
+            liczba = liczba*3 + 1
+            #print (liczba)
+            dlugosc += 1
+        if liczba == 1:
+        # print ("stop")
+            print (f"długość ciągu Colatza to {dlugosc} liczb")
+            break
+else:
+     print ("podano złą liczbę")
