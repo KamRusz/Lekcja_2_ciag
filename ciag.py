58428d19bd66ecf10c41ccbfcaf9362b63b960bc
Kamil <kamilrusztyn@gmail.com>
2022-01-02 12:46:13 +0100

    podaje dlugosc ciagu zamiast drukowac wartosci

--- ciag.py
+++ ciag.py
@@ -1,12 +1,16 @@
 print ('Podaj liczbę od 1 do 100')
 liczba = int(input())
+dlugosc = 1
 while liczba:
     if not liczba % 2:
         liczba = liczba /2
-        print (liczba)
+       # print (liczba)
+        dlugosc += 1
     elif liczba % 2:
         liczba = liczba*3 + 1
-        print (liczba)
+        #print (liczba)
+        dlugosc += 1
     if liczba == 1:
-        print ("stop")
+       # print ("stop")
+        print (f"długość ciągu Colatza to {dlugosc} liczb")
         break
